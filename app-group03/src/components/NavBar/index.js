3b728765da37a855b1c7f211d381ae38b752f50d
Marcello Victor <marcello@TEPLACONTABIL.LOCAL>
2023-06-26 17:56:55 -0300

    Iniciando a NavBar

--- app-group03/src/components/NavBar/index.js
+++ app-group03/src/components/NavBar/index.js
@@ -1,5 +1,24 @@
 export default function NavBar() {
     return (
-        <div>NavBar</div>
+        <header>
+            <div className="topo">
+                <div className="logo">
+                    <i>Logo</i>
+                    Digital Store
+                </div>
+                <div className="search">
+                    <input placeholder="Pesquisar produto..."/>
+                    <button className="icon-button">
+                        <i>Procurar</i>
+                    </button>
+                </div>
+                <div className="topo-direito">
+                    <a href="">Cadastra-se</a>
+                    <button className="btn-primary">Entrar</button>
+                    <button className="icon-button"><i>Carrinho de compras</i></button>
+                </div>
+            </div>
+        </header>
+        
     )
 }
