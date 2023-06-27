1805c25f8c54df3d1f56fa252b699b7c5487f4e8
Marcello Victor <marcello@TEPLACONTABIL.LOCAL>
2023-06-27 13:00:16 -0300

    update layout by navbar

--- app-group03/src/components/NavBar/index.js
+++ app-group03/src/components/NavBar/index.js
@@ -1,24 +1,33 @@
+import {Link} from "react-router-dom";
+import {Box, Button, Container, TextField} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import Logo from './img/dc-logo.png';
+import './styles.scss';
+
 export default function NavBar() {
     return (
-        <header>
-            <div className="topo">
-                <div className="logo">
-                    <i>Logo</i>
-                    Digital Store
-                </div>
-                <div className="search">
-                    <input placeholder="Pesquisar produto..."/>
-                    <button className="icon-button">
-                        <i>Procurar</i>
-                    </button>
-                </div>
-                <div className="topo-direito">
-                    <a href="">Cadastra-se</a>
-                    <button className="btn-primary">Entrar</button>
-                    <button className="icon-button"><i>Carrinho de compras</i></button>
-                </div>
-            </div>
-        </header>
-        
+        <div className="navbar">
+            <Container fixed>
+                <Box sx={{justifyContent: 'space-between', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    <div>
+                        <div className="navbar-logo">
+                            <img src={Logo}/>
+                            <Link to='/'><h1>Digital Store</h1></Link>
+                        </div>
+                       
+                    </div>
+                    <div className="input-text">
+                        <TextField id="outlined-basic" label="Digite aqui para pesquisar..." variant="outlined" size="small"/>
+                        <div className="search-click">
+                            <span><SearchIcon/></span>
+                        </div>
+                    </div>
+                    <Link to='/'><span>Cadastra-se</span></Link>
+                    <Link to='/'><Button variant="contained">Entrar</Button></Link>
+                    <Link className="shoppingCart" to='/'><ShoppingCartOutlinedIcon/></Link>
+                </Box>
+            </Container>
+        </div>        
     )
 }
